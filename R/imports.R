073db0f29763ee1d6aa3bec0ac792aaa8ac0d059
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-10-08 19:41:31 +0200

    update materials, render rmds in gha

--- R/imports.R
+++ R/imports.R
@@ -1,23 +1,23 @@
 #' Internal tidyverse methods
-#'
+#' @name tidyverse
 #' @import tidyverse
 #' @keywords internal
 NULL
 
 #' Internal broom methods
-#'
+#' @name broom
 #' @import broom
 #' @keywords internal
 NULL
 
 #' Internal palmerpenguins methods
-#'
+#' @name palmerpenguins
 #' @import palmerpenguins
 #' @keywords internal
 NULL
-#
+
 #' Internal learnr methods
-#'
+#' @name learnr
 #' @import learnr
 #' @keywords internal
 NULL
--- data-raw/make-gifs.R
+++ data-raw/make-gifs.R
@@ -11,14 +11,14 @@ dt <- tibble(
   col1 = 1, col2 = 2, col3 = 3, col4 = 4, col5 = 5
 )
 
-dt %>%
-  mutate(panel = 1) %>%
-  gather(key, columns, contains("col")) %>%
-  bind_rows( filter(dt, fill == "keep") %>%
+dt |>
+  mutate(panel = 1) |>
+  gather(key, columns, contains("col")) |>
+  bind_rows( filter(dt, fill == "keep") |>
                mutate(panel = 2,
-                      rows = row_number()) %>%
+                      rows = row_number()) |>
       gather(key, columns, contains("col"))
-  ) %>%
+  ) |>
 
 ggplot(aes(x = columns, y = rows)) +
   geom_tile(aes(fill = fill),
@@ -44,19 +44,19 @@ anim_save("filtering.gif",
 dt <- tibble(
   rows = 1:8,
   col1 = 1, col2 = 2, col3 = 3, col4 = 4, col5 = 5
-) %>%
+) |>
   gather(key, columns, contains("col"))
 
-dt %>%
+dt |>
   mutate(panel = 1,
-         fill = ifelse(key %in% paste0("col", c(1,2,4)), "keep", "discard")) %>%
+         fill = ifelse(key %in% paste0("col", c(1,2,4)), "keep", "discard")) |>
   bind_rows(
-    filter(dt, key %in% paste0("col", c(1,2,4))) %>%
+    filter(dt, key %in% paste0("col", c(1,2,4))) |>
       mutate(panel = 2,
              key = ifelse(key == "col4", "col3", key),
              columns = ifelse(columns == 4, 3, columns),
              fill = "keep")
-  ) %>%
+  ) |>
 
   ggplot(aes(x = columns, y = rows)) +
   geom_tile(aes(fill = fill),
@@ -84,24 +84,24 @@ dt <- tibble(
   subject = factor(rep(1:2, each = 2)),
   rows = c(1:2, 3:4),
   col1 = 1, col2 = 2, col3 = 3
-) %>%
-  gather(key, columns, contains("col")) %>%
+) |>
+  gather(key, columns, contains("col")) |>
   mutate(panel = 1)
 
-tall_dt <- mutate(dt, panel = 2) %>%
+tall_dt <- mutate(dt, panel = 2) |>
   bind_rows(
     mutate(dt,
            panel = 2,
            columns = columns + 3)
-  ) %>%
-  group_by(subject, columns, panel) %>%
-  summarise(rows = 1) %>%
-  ungroup() %>%
+  ) |>
+  group_by(subject, columns, panel) |>
+  summarise(rows = 1) |>
+  ungroup() |>
   mutate(rows = ifelse(subject == 1, 1, 2))
 
 
-dt %>%
-  bind_rows(tall_dt)  %>%
+dt |>
+  bind_rows(tall_dt)  |>
 
   ggplot(aes(x = columns, y = rows)) +
   geom_tile(aes(fill = subject),
